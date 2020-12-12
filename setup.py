d802775c2d7ec35af61800511f7bc3dc47eb4429
Sebuhi <sabuhi.shukurov@gmail.com>
2020-12-12 18:39:44 +0400

    realase test

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r") as fh:
 setup(
     name="madmigration",
     packages=find_packages(),
+    version="0.0.1",
     entry_points="""
         [console_scripts]
         madmigrate=madmigration.scripts.commands:cli
     """,
     description="Mad migration",
+    url="https://github.com/MadeByMads/mad-migration",
     long_description=long_description,
-    
-    install_requires=["click", "SQLAlchemy<=1.3.18", ""],
+    install_requires=["click>=7.1.2","SQLAlchemy>=1.3.18","mysqlclient>=2.0.1","psycopg2>=2.8.5","alembic>=1.4.2", "PyYAML>=5.3.1 "],
     classifiers=[
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
@@ -32,6 +33,7 @@ setup(
         'pytest',
         'mock',
     ],
+    platforms=['any'],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.* '
 )
 
