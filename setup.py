b35d86a7b31f82fe0de96550ec9cf60dd484ab7c
Sebuhi <sabuhi.shukurov@gmail.com>
2020-12-12 19:17:21 +0400

    testing setup.py again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name="madmigration",
     packages=find_packages(),
-    version="0.1.1",
+    version="0.1.2",
     entry_points="""
         [console_scripts]
         madmigrate=madmigration.scripts.commands:cli
