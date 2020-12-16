bd9ea605a0d0aabc845c233e1d874125e7b8c612
Tural <tural_m@hotmail.com>
2020-12-16 15:26:51 +0400

    remove unused imports

--- madmigration/basemigration/base.py
+++ madmigration/basemigration/base.py
@@ -7,38 +7,8 @@ from madmigration.errors import TableExists
 from sqlalchemy.ext.mutable import MutableDict
 from collections import defaultdict
 from madmigration.postgresqldb.type_convert import get_type_object
-from sqlalchemy.dialects.postgresql import (
-    ARRAY,
-    BIGINT,
-    BIT,
-    BOOLEAN,
-    BYTEA,
-    CHAR,
-    DATE,
-    # DOUBLE_PRECISION,
-    # ENUM,
-    FLOAT,
-    INET,
-    INTEGER,
-    # INTERVAL,
-    JSON,
-    JSONB,
-    MACADDR,
-    MONEY,
-    NUMERIC,
-    OID,
-    REAL,
-    SMALLINT,
-    TEXT,
-    TIME,
-    TIMESTAMP,
-    VARCHAR,
-)
-from sqlalchemy import DateTime
-from sqlalchemy_utils import UUIDType
 from madmigration.config.conf import Config
 from madmigration.db_operations.operations import DbOperations
-from pprint import pprint
 
 
 class BaseMigrate: 
@@ -68,8 +38,6 @@ class BaseMigrate:
         """ collects all tables that the program should create """
         try:
             for migrate_table in self.migration_tables:
-                # pprint(migrate_table.dict().keys())
-                
                 tabel_name = migrate_table.migrationTable.DestinationTable.name
                 self.table_list.add(tabel_name)
         except Exception as err:
@@ -329,33 +297,6 @@ class BaseMigrate:
         :param type_name: str
         :return: object class
         """
-        return {
-            "varchar": VARCHAR,
-            "char": CHAR,
-            "string": VARCHAR,
-            "text": TEXT,
-            "integer": INTEGER,
-            "smallint": SMALLINT,
-            "bigint": BIGINT,
-            "binary": BYTEA,
-            "boolean": BOOLEAN,
-            "bool": BOOLEAN,
-            "date": DATE,
-            "datetime": DateTime,
-            "timestamp": TIMESTAMP,
-            "time": TIME,
-            # "enum": ENUM,
-            "float": FLOAT,
-            "real": REAL,
-            "json": MutableDict.as_mutable(JSON),
-            "jsonb": MutableDict.as_mutable(JSONB),
-            # "array": ARRAY, #FIXME column with array include array elemet type argument
-            "numeric": NUMERIC,
-            "money": MONEY,
-            "macaddr": MACADDR,
-            "inet": INET,
-            "oid": OID,
-            "uuid": UUIDType(binary=False),
-        }.get(type_name.lower())
+        raise NotImplementedError
 
 
