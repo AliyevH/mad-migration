13d82a7c71caeecd98933b003adbf0c76debcdfd
Hasan <hasan.aliyev.555@gmail.com>
2020-12-28 21:05:28 +0400

    logger added

---
+++ madmigration/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+import logging.config
+import yaml
+import os 
+import coloredlogs
+
+mad_path = os.path.abspath(os.curdir)
+config_file = mad_path + "/madmigration/config/logger_config.yml"
+
+
+
+
+def configure_logging():
+    with open(config_file) as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        fmt = config.get("formatters").get("simple")
+        coloredlogs.install(level="DEBUG", **fmt)
