816cbae172ffa5625ddc24bd0a1ceb0c721d635a
Tural <tural_m@hotmail.com>
2021-01-21 12:51:36 +0400

    add version to cli

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 import madmigration
+from madmigration.utils.helpers import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name="madmigration",
     packages=find_packages(),
-    version="0.1.7",
+    version=__version__,
     entry_points="""
         [console_scripts]
         madmigrate=madmigration.scripts.commands:cli
